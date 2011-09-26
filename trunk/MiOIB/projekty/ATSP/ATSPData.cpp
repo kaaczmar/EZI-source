14e27fa0bd9378feb6dfb85243f207e23be5d821
artur.a.jaworski@gmail.com <artur.a.jaworski@gmail.com@513d43aa-3f5d-df24-0f85-0aaa06387b59>
2011-09-26 19:28:33 +0000

    Pierwsza wersja działającego parsera

--- trunk/MiOIB/projekty/ATSP/ATSPData.cpp
+++ trunk/MiOIB/projekty/ATSP/ATSPData.cpp
@@ -6,7 +6,29 @@
  */
 
 #include <ATSPData.hpp>
+#include <iostream>
 
-ATSPData::ATSPData(){
+using namespace boost;
+using namespace std;
 
+ATSPData::ATSPData() {
+
+}
+
+void ATSPData::setDimension(int dimension){
+	this->dimension = dimension;
+	data.resize(extents[dimension][dimension]);
+}
+
+int ATSPData::getDimension(){
+	return dimension;
+}
+
+void ATSPData::print(){
+	for (int y = 0; y < dimension; y++){
+		for (int x = 0; x < dimension; x++){
+			cout << data[x][y] << ",";
+		}
+		cout << endl;
+	}
 }
