49319e7952704bbbe313408854c35f06a6e04f26
hrkm@poczta.fm <hrkm@poczta.fm@513d43aa-3f5d-df24-0f85-0aaa06387b59>
2011-10-04 12:49:48 +0000

    document class for storing "documents"... obvious? main - test single document

---
+++ trunk/EZI/TF-IDF/src/ezi/tf_idf/data/Document.java
@@ -0,0 +1,36 @@
+package ezi.tf_idf.data;
+
+import ezi.tf_idf.utils.Stemmer;
+
+public class Document {
+	private String title;
+	private String content;
+	private String stemmedDocument;
+	
+	public String getTitle() { return title; }
+	public String getContent() { return content; }
+	public String getStemmedDocument() { return stemmedDocument; }
+	
+	public Document(String title, String content)
+	{
+		this.title = title;
+		this.content = content;
+		stemmedDocument = "";
+		
+		Stemmer s = new Stemmer();
+		String temp = title + "\n" + content;
+		for (int i = 0; i < temp.length(); i++)
+		{
+			char ch = temp.charAt(i);
+			if (Character.isLetter(ch)) s.add(ch);
+			else if (Character.isWhitespace(ch))
+			{
+				s.stem();
+				stemmedDocument += s.toString() + " ";
+			}
+		}
+		s.stem();
+		stemmedDocument += s.toString();
+		stemmedDocument = stemmedDocument.replaceAll("[ \t\n]+", " ");
+	}
+}
