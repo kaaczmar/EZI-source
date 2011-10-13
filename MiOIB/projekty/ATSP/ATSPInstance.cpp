ff3d94ecfcce13e9c51e046d0c0f1ed1e3bae30b
Artur.A.Jaworski <artur.a.jaworski@gmail.com>
2011-10-13 11:31:00 +0000

    Popra... a lot of shit

--- MiOIB/projekty/ATSP/ATSPInstance.cpp
+++ MiOIB/projekty/ATSP/ATSPInstance.cpp
@@ -16,8 +16,10 @@ using namespace std;
 ATSPInstance::ATSPInstance(unsigned int length) :
 	length(length) {
 	instance = new unsigned int[length];
+	neighbour = new unsigned int[length];
 	for (unsigned int i = 0; i < length; i++) {
 		instance[i] = i;
+		neighbour[i] = i;
 	}
 }
 
@@ -28,6 +30,12 @@ ATSPInstance::ATSPInstance(ATSPInstance &instance){
 	for (unsigned int i = 0; i < this->length; i++)
 		this->instance[i] = instance.instance[i];
 
+}
+
+void ATSPInstance::swapNeighbour(unsigned int x, unsigned int y) {
+	unsigned int temp = neighbour[y];
+	neighbour[y] = neighbour[x];
+	neighbour[x] = temp;
 }
 
 void ATSPInstance::swap(unsigned int x, unsigned int y) {
@@ -41,6 +49,13 @@ void ATSPInstance::show() {
 		cout << instance[i] << "-";
 	}
 	cout << instance[length - 1] << endl;
+}
+
+void ATSPInstance::showNeighbour() {
+	for (unsigned int i = 0; i < length - 1; i++) {
+		cout << neighbour[i] << "-";
+	}
+	cout << neighbour[length - 1] << endl;
 }
 
 void ATSPInstance::randomize() {
@@ -60,12 +75,15 @@ void ATSPInstance::reinitializeNeighbourhood(){
 	swapX = 0;
 	swapY = 1;
 	firstNeighbour = true;
+
+	for (unsigned int i = 0; i < length; i++)
+		neighbour[i] = instance[i];
 }
 
 bool ATSPInstance::nextNeighbour(){
 	if (!firstNeighbour){
 		//Zmiana powrotna
-		swap(swapX,swapY);
+		swapNeighbour(swapX,swapY);
 		//Nowe indeksy do zamiany
 		if (++swapY == length)
 			swapY = ++swapX + 1;
@@ -75,14 +93,11 @@ bool ATSPInstance::nextNeighbour(){
 		return false;
 
 	firstNeighbour = false;
-	swap(swapX, swapY);
+	swapNeighbour(swapX, swapY);
 
 	return true;
 }
 
-void ATSPInstance::resetInstanceToBase(){
-	if (firstNeighbour)
-		return;
-
-	swap(swapX,swapY);
+unsigned int *ATSPInstance::getCurrentNeighbour(){
+	return neighbour;
 }
