a9dafa82e331b6038a3dcf91c519fb5a45033747
Artur.A.Jaworski <artur.a.jaworski@gmail.com>
2011-11-01 20:57:32 +0000

    Rozszerzanie zapytań - przekazywanie parametrów bangla.

--- EZI/TF-IDF/src/ezi/tf_idf/QueryExpansionDialog.java
+++ EZI/TF-IDF/src/ezi/tf_idf/QueryExpansionDialog.java
@@ -3,11 +3,17 @@ package ezi.tf_idf;
 import java.util.ArrayList;
 
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.*;
+import javax.swing.JButton;
+
+import ezi.tf_idf.data.Extension;
+import ezi.tf_idf.data.Keyword;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class QueryExpansionDialog extends JDialog {
 	/**
@@ -16,7 +22,11 @@ public class QueryExpansionDialog extends JDialog {
 	private static final long serialVersionUID = 2800804400986018447L;
 	private JTable table;
 	private DefaultTableModel tableModel;
-	boolean updatingTable;
+	private JButton btnSearch;
+	private JButton btnCancel;
+	private boolean result = false;
+	
+	private ArrayList<Extension> extensions;
 
 	/**
 	 * Launch the application.
@@ -31,34 +41,47 @@ public class QueryExpansionDialog extends JDialog {
 	// }
 	// }
 
+	public QueryExpansionDialog(JFrame owner){
+		super(owner);
+		initGUI();
+	}
+	
 	/**
 	 * Create the dialog.
 	 */
 	public QueryExpansionDialog() {
+		initGUI();
+	}
+	
+	private void initGUI(){
+		setTitle("Query expansion");
 		setBounds(100, 100, 500, 600);
 		getContentPane().setLayout(null);
+		
+		setModal(true);
 
-		table = new JTable() {
-			private static final long serialVersionUID = -1978493142990925510L;
-			// public TableCellRenderer getCellRenderer(int row, int column){
-			// if (column == 1)
-			// return new CustomTableCellRenderers.SpinnerCellRenderer();
-			// else
-			// return new DefaultTableCellRenderer();
-			// }
-		};
+		table = new JTable();
 
 		tableModel = new DefaultTableModel(new Object[][] {}, new String[] {
-				"Word", "Weight", "Weight Norm" }) {
+				"Word", "Weight", "Use" }) {
 			private static final long serialVersionUID = 7070720547880801555L;
-			boolean[] columnEditables = new boolean[] { false, true, false };
+			boolean[] columnEditables = new boolean[] { false, true, true };
 
 			public boolean isCellEditable(int row, int column) {
 				return columnEditables[column];
 			}
+			
+			Class[] columnTypes = new Class[] {
+				String.class, Double.class, Boolean.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			
 		};
 
 		table.setModel(tableModel);
+		
 		table.getColumnModel().getColumn(0).setResizable(false);
 		table.getColumnModel().getColumn(0).setPreferredWidth(255);
 		table.getColumnModel().getColumn(0).setMinWidth(255);
@@ -73,15 +96,9 @@ public class QueryExpansionDialog extends JDialog {
 		table.getColumnModel().getColumn(2).setMaxWidth(100);
 
 		table.setRowHeight(40);
-
-		tableModel.addTableModelListener(new TableModelListener() {
-			@Override
-			public void tableChanged(TableModelEvent e) {
-				if (updatingTable)
-					return;
-				countNormalizedWeights();
-			}
-		});
+		
+		table.getTableHeader().setReorderingAllowed(false);
+		table.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
 
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setBounds(12, 12, 474, 506);
@@ -89,59 +106,49 @@ public class QueryExpansionDialog extends JDialog {
 				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
 		getContentPane().add(scrollPane);
-
-	}
-
-	private void countNormalizedWeights() {
-		updatingTable = true;
-
-		double max = Double.MIN_VALUE;
-		double min = Double.MAX_VALUE;
-
-		for (int i = 0; i < tableModel.getRowCount(); i++) {
-			try {
-				double value = Double.parseDouble((String) tableModel
-						.getValueAt(i, 1));
-
-				if (value < min)
-					min = value;
-				if (value > max)
-					max = value;
-
-			} catch (NumberFormatException e1) {
-				System.err.println("Invalid number");
-				return;
+		
+		btnSearch = new JButton("Search");
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				collectResults();
+				result = true;
+				setVisible(false);
 			}
-		}
-
-		if (min < 0) {
-			System.err.println("Weight can not be lower than 0");
-			return;
-		}
-
-		if (max == 0)
-			return;
-
-		for (int i = 0; i < tableModel.getRowCount(); i++) {
-			try {
-				double value = Double.parseDouble((String) tableModel
-						.getValueAt(i, 1));
-
-				tableModel.setValueAt(Math.round((100 * value) / max) / 100.0,
-						i, 2);
-			} catch (NumberFormatException e1) {
-				System.err.println("Invalid number");
+		});
+		btnSearch.setBounds(326, 530, 117, 25);
+		getContentPane().add(btnSearch);
+		
+		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				result = false;
+				setVisible(false);
 			}
-		}
-
-		updatingTable = false;
+		});
+		btnCancel.setBounds(38, 530, 117, 25);
+		getContentPane().add(btnCancel);
 	}
 
 	public void initTable(ArrayList<String> words) {
 		tableModel.setRowCount(0);
 
 		for (String s : words)
-			tableModel.addRow(new Object[] { s, "0", "0" });
+			tableModel.addRow(new Object[] { s, 0.0, new Boolean(false) });
 	}
-
+	
+	public boolean getResult(){
+		return result;
+	}
+	
+	public ArrayList<Extension> getExtensions(){
+		return extensions;
+	}
+	
+	private void collectResults(){
+		extensions = new ArrayList<Extension>();
+		for (int i = 0; i < tableModel.getRowCount(); i++){
+			if ((Boolean)table.getValueAt(i, 2))
+				extensions.add(new Extension(new Keyword((String)table.getValueAt(i, 0)), (Double) table.getValueAt(i, 1)));
+		}
+	}
 }
