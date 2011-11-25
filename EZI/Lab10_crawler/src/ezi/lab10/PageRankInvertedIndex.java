d9235b66f59bf50f1b99ed108fa1801c301b38b5
Artur.A.Jaworski <artur.a.jaworski@gmail.com>
2011-11-25 22:48:39 +0000

    Zrobione EZI

--- EZI/Lab10_crawler/src/ezi/lab10/PageRankInvertedIndex.java
+++ EZI/Lab10_crawler/src/ezi/lab10/PageRankInvertedIndex.java
@@ -1,17 +1,137 @@
 package ezi.lab10;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import ir.utilities.DoubleValue;
+import ir.utilities.Weight;
+import ir.vsr.DocumentIterator;
+import ir.vsr.DocumentReference;
+import ir.vsr.HashMapVector;
 import ir.vsr.InvertedIndex;
+import ir.vsr.Retrieval;
 
-public class PageRankInvertedIndex extends InvertedIndex{
+public class PageRankInvertedIndex extends InvertedIndex {
 
-	//Mozna wygenerowac tez drugi konstruktor
-	
+	private double weight = 0;
+	private HashMap<String, Double> pageRanks = null;
+
 	public PageRankInvertedIndex(File dirFile, short docType, boolean stem,
-			boolean feedback) {
+			boolean feedback, double weight) {
 		super(dirFile, docType, stem, feedback);
-		// TODO Auto-generated constructor stub
+		this.weight = weight;
+		loadPageRanks();
+	}
+	
+	private void loadPageRanks(){
+		pageRanks = new HashMap<String, Double>();
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(dirFile.getAbsolutePath()+"/PageRanks.txt")));
+			
+			String line = null;
+			while (( line = br.readLine()) != null){
+				String[] values = line.split(" ");
+				pageRanks.put(values[0], Double.parseDouble(values[1]));
+	        }
+			
+			br.close();
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("PageRanks file not found in directory");
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/** Perform ranked retrieval on this input query Document vector. */
+	@Override
+    public Retrieval[] retrieve(HashMapVector vector) {
+	// Create a hashtable to store the retrieved documents.  Keys
+	// are docRefs and values are DoubleValues which indicate the
+	// partial score accumulated for this document so far.
+	// As each token in the query is processed, each document
+	// it indexes is added to this hashtable and its retrieval
+	// score (similarity to the query) is appropriately updated.
+	HashMap retrievalHash = new HashMap();
+	// Initialize a variable to store the length of the query vector
+	double queryLength = 0.0;
+	// Iterate through each token in the query input Document
+	Iterator mapEntries = vector.iterator();
+	while (mapEntries.hasNext()) {
+	    // Get the token and the count for each token in the query
+	    Map.Entry entry = (Map.Entry)mapEntries.next();
+	    String token = (String)entry.getKey();
+	    double count = ((Weight)entry.getValue()).getValue();
+	    // Determine the score added to the similarity of each document
+	    // indexed under this token and update the length of the
+	    // query vector with the square of the weight for this token.
+	    queryLength = queryLength + incorporateToken(token, count, retrievalHash);
+	}
+	// Finalize the length of the query vector by taking the square-root of the
+	// final sum of squares of its token wieghts.
+	queryLength = Math.sqrt(queryLength);
+	// Make an array to store the final ranked Retrievals.
+	Retrieval[] retrievals = new Retrieval[retrievalHash.size()];
+	// Iterate through each of the retreived docuements stored in
+	// the final retrievalHash.
+	Iterator rmapEntries = retrievalHash.entrySet().iterator();
+	int retrievalCount = 0;
+	while (rmapEntries.hasNext()) {
+	    // Get the DocumentReference and score for each retrieved document
+	    Map.Entry entry = (Map.Entry)rmapEntries.next();
+	    DocumentReference docRef = (DocumentReference)entry.getKey();
+	    double score = ((DoubleValue)entry.getValue()).value;
+	    // Normalize score for the lengths of the two document vectors
+	    score = score / (queryLength * docRef.length);  
+	    score = score + (weight * pageRanks.get(docRef.file.getName()));
+	    // Add a Retrieval for this document to the result array
+	    retrievals[retrievalCount++] = new Retrieval(docRef, score);
+	}
+	// Sort the retrievals to produce a final ranked list using the
+	// Comparator for retrievals that produces a best to worst ordering.
+	Arrays.sort(retrievals);
+	return retrievals;
+    }
+
+	public static void main(String[] args) {
+		// Parse the arguments into a directory name and optional flag
+		String dirName = args[args.length - 1];
+		short docType = DocumentIterator.TYPE_HTML;
+		boolean stem = false, feedback = false;
+		double weight = 2;
+		int i = 0;
+		while (i < args.length - 1) {
+			String flag = args[i];
+			if (flag.equals("-stem"))
+				// Stem tokens with Porter stemmer
+				stem = true;
+			else if (flag.equals("-feedback"))
+				// Use relevance feedback
+				feedback = true;
+			else if (flag.equals("-weight")) {
+				i++;
+				weight = Double.parseDouble(args[i]);
+			} else {
+				System.out.println("\nUnknown flag: " + flag);
+				System.exit(1);
+			}
+			i++;
+		}
+		// Create an inverted index for the files in the given directory.
+		PageRankInvertedIndex index = new PageRankInvertedIndex(new File(
+				dirName), docType, stem, feedback, weight);
+		// index.print();
+		// Interactively process queries to this index.
+		index.processQueries();
 	}
 
 }
--- EZI/Lab10_crawler/src/ezi/lab10/PageRankSpider.java
+++ EZI/Lab10_crawler/src/ezi/lab10/PageRankSpider.java
@@ -1,11 +1,99 @@
 package ezi.lab10;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.print.attribute.standard.PagesPerMinute;
+
+import ir.utilities.MoreMath;
+import ir.utilities.MoreString;
+import ir.webutils.Graph;
+import ir.webutils.HTMLPage;
+import ir.webutils.Link;
+import ir.webutils.Node;
+import ir.webutils.PathDisallowedException;
 import ir.webutils.Spider;
 
 public class PageRankSpider extends Spider {
+	private Graph pageGraph = null;
+	private HashMap<Link, String> pageMap = null;
+	private List<HTMLPage> pages = null;
 
-	
-	
+	@Override
+	public void doCrawl() {
+		pageMap = new HashMap<Link, String>();
+		visited = new HashSet();
+		pages = new ArrayList<HTMLPage>();
+		
+		while (linksToVisit.size() > 0 && count < maxCount) {
+			// Pause if in slow mode
+			if (slow) {
+				synchronized (this) {
+					try {
+						wait(1000);
+					} catch (InterruptedException e) {
+					}
+				}
+			}
+			// Take the top link off the queue
+			Link link = (Link) linksToVisit.remove(0);
+			System.out.println("Trying: " + link);
+			// Skip if already visited this page
+			if (!visited.add(link)) {
+				System.out.println("Already visited");
+				continue;
+			}
+			if (!linkToHTMLPage(link)) {
+				System.out.println("Not HTML Page");
+				continue;
+			}
+			HTMLPage currentPage = null;
+			// Use the page retriever to get the page
+			try {
+				currentPage = webpr.getHTMLPage(link);
+			} catch (PathDisallowedException e) {
+				System.out.println("Robots excluded from access to" + link
+						+ ": " + e);
+				continue;
+			}
+			if (currentPage.empty()) {
+				System.out.println("No Page Found");
+				continue;
+			}
+			if (currentPage.indexAllowed()) {
+				count++;
+				System.out.println("Indexing" + "(" + count + "): " + link);
+				processPage(currentPage);
+				pageMap.put(currentPage.getLink(), "P" +  MoreString.padWithZeros(count,(int)Math.floor(MoreMath.log(maxCount, 10)) + 1) + ".html");
+				pages.add(currentPage);
+				
+			}
+			if (count < maxCount) {
+				List newLinks = getNewLinks(currentPage);
+				// System.out.println("Adding the following links" + newLinks);
+				// Add new links to end of queue
+				linksToVisit.addAll(newLinks);
+			}
+			
+		}
+		
+		pageGraph = new Graph();
+		for (HTMLPage page : pages){
+			for (Object l : page.getOutLinks()){
+				pageGraph.addEdge(pageMap.get(page.getLink()), pageMap.get(l));
+			}
+		}
+		System.out.println("\nPage graph structure:");
+		pageGraph.print();
+
+		PageRankFile pageRankFile = new PageRankFile(saveDir.getAbsolutePath());
+		
+		System.out.println("\nPage ranks:");
+		pageRankFile.calculate(pageGraph, 0.15, 50);
+	}
+
 	public static void main(String args[]) {
 		new PageRankSpider().go(args);
 	}
